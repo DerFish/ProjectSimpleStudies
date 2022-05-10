5d61873e9823d8df2a332453ab8e5f68e4050cec
Niggl03 <99817086+Niggl03@users.noreply.github.com>
2022-05-10 11:05:42 +0200

    web

--- SimpleStudies/Views/Stundenplan.cs
+++ SimpleStudies/Views/Stundenplan.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
 
 namespace SimpleStudies.Views
 {
@@ -15,7 +17,33 @@ namespace SimpleStudies.Views
         public Stundenplan()
         {
             InitializeComponent();
+            InitializeBrowser();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private async Task Initizated()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+        }
+        public async void InitializeBrowser()
+        {
+            Uri uri = new Uri("https://rapla.dhbw.de/rapla/calendar?key=25q8zGuMAw3elezlMsiegXs3Z-sCY45qHbigy7wiQ2e27FEEw1gUZrt95IawaK3jxZy_Y5bukYcuFWfh6SXaWScIBj1vU-rKPi1EJOM9QFX4p3tKwLad0qDpHO7Zn5W-IUQ1RhEYxI8ckde9URa_ng&salt=-2070726140");
+            await Initizated();
+            webView21.CoreWebView2.Navigate(uri.ToString());
+        }
+        private void webView21_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            this.Text = e.Uri.ToString() + "Is loading . . .";
+        }
+
 
     }
 }
+
