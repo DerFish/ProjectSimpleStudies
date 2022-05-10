73cde0048c9dc3cf6fe94a36a7400b47e57c04f9
Mxrcx02 <cb.me@gmx.de>
2022-05-10 11:41:07 +0200

    Notenübersicht Button

--- SimpleStudies/Views/Sitepanel.cs
+++ SimpleStudies/Views/Sitepanel.cs
@@ -22,5 +22,11 @@ namespace SimpleStudies.Views
             Stundenplan stundenplan = new Stundenplan();
             stundenplan.Show();
         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Notenübersicht notenübersicht = new Notenübersicht();
+            notenübersicht.Show();
+        }
     }
 }
