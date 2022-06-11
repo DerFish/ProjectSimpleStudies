f015bb382ccbb91e6e10ca46ac059992f53b53d2
Matthias Lay <m.lay@groninger.de>
2022-06-11 09:56:46 +0200

    added setup

--- SimpleStudies/Controls/NewsItemControl.Designer.cs
+++ SimpleStudies/Controls/NewsItemControl.Designer.cs
@@ -48,9 +48,9 @@
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 42F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(324, 294);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(240, 193);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // PbMain
@@ -59,7 +59,7 @@
             this.PbMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PbMain.Location = new System.Drawing.Point(3, 3);
             this.PbMain.Name = "PbMain";
-            this.PbMain.Size = new System.Drawing.Size(318, 114);
+            this.PbMain.Size = new System.Drawing.Size(234, 114);
             this.PbMain.TabIndex = 0;
             this.PbMain.TabStop = false;
             this.PbMain.WaitOnLoad = true;
@@ -73,7 +73,7 @@
             this.LblTitle.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.LblTitle.Location = new System.Drawing.Point(3, 120);
             this.LblTitle.Name = "LblTitle";
-            this.LblTitle.Size = new System.Drawing.Size(318, 80);
+            this.LblTitle.Size = new System.Drawing.Size(234, 42);
             this.LblTitle.TabIndex = 1;
             this.LblTitle.Text = "label1";
             this.LblTitle.Click += new System.EventHandler(this.LblTitle_Click);
@@ -81,9 +81,10 @@
             // LblAbstract
             // 
             this.LblAbstract.AutoSize = true;
-            this.LblAbstract.Location = new System.Drawing.Point(3, 200);
+            this.LblAbstract.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblAbstract.Location = new System.Drawing.Point(3, 162);
             this.LblAbstract.Name = "LblAbstract";
-            this.LblAbstract.Size = new System.Drawing.Size(38, 15);
+            this.LblAbstract.Size = new System.Drawing.Size(38, 13);
             this.LblAbstract.TabIndex = 2;
             this.LblAbstract.Text = "label1";
             // 
@@ -91,9 +92,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "NewsItemControl";
-            this.Size = new System.Drawing.Size(324, 294);
+            this.Size = new System.Drawing.Size(240, 193);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PbMain)).EndInit();
